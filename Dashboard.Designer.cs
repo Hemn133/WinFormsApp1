e06d18168378f991f9be926436c0b1f7be717b33
EZi CO <aryan.golden22@gmail.com>
2025-01-10 06:49:27 +0300

    FullScreen

--- Dashboard.Designer.cs
+++ Dashboard.Designer.cs
@@ -39,15 +39,17 @@
             button7 = new Button();
             button8 = new Button();
             pictureBox1 = new PictureBox();
+            panel2 = new Panel();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.WhiteSmoke;
-            panel1.Location = new Point(472, 199);
+            panel1.Location = new Point(26, 26);
             panel1.Name = "panel1";
-            panel1.Size = new Size(986, 642);
+            panel1.Size = new Size(1616, 1029);
             panel1.TabIndex = 2;
             // 
             // button1
@@ -58,7 +60,7 @@
             button1.ForeColor = Color.Teal;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(1464, 288);
+            button1.Location = new Point(17, 92);
             button1.Name = "button1";
             button1.Size = new Size(243, 61);
             button1.TabIndex = 4;
@@ -71,7 +73,7 @@
             // 
             button2.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button2.ForeColor = Color.Teal;
-            button2.Location = new Point(1464, 355);
+            button2.Location = new Point(17, 159);
             button2.Name = "button2";
             button2.Size = new Size(243, 61);
             button2.TabIndex = 5;
@@ -84,7 +86,7 @@
             // 
             button3.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button3.ForeColor = Color.Teal;
-            button3.Location = new Point(1464, 422);
+            button3.Location = new Point(17, 226);
             button3.Name = "button3";
             button3.Size = new Size(243, 61);
             button3.TabIndex = 6;
@@ -97,7 +99,7 @@
             // 
             button4.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button4.ForeColor = Color.Teal;
-            button4.Location = new Point(1464, 489);
+            button4.Location = new Point(17, 293);
             button4.Name = "button4";
             button4.Size = new Size(243, 61);
             button4.TabIndex = 7;
@@ -110,7 +112,7 @@
             // 
             button5.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button5.ForeColor = Color.Teal;
-            button5.Location = new Point(1464, 556);
+            button5.Location = new Point(17, 360);
             button5.Name = "button5";
             button5.Size = new Size(243, 61);
             button5.TabIndex = 8;
@@ -123,7 +125,7 @@
             // 
             button6.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button6.ForeColor = Color.Teal;
-            button6.Location = new Point(1464, 623);
+            button6.Location = new Point(17, 427);
             button6.Name = "button6";
             button6.Size = new Size(243, 61);
             button6.TabIndex = 9;
@@ -136,7 +138,7 @@
             // 
             button7.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button7.ForeColor = Color.Teal;
-            button7.Location = new Point(1464, 690);
+            button7.Location = new Point(17, 494);
             button7.Name = "button7";
             button7.Size = new Size(243, 61);
             button7.TabIndex = 10;
@@ -149,7 +151,7 @@
             // 
             button8.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             button8.ForeColor = Color.Red;
-            button8.Location = new Point(1464, 780);
+            button8.Location = new Point(17, 584);
             button8.Name = "button8";
             button8.Size = new Size(243, 61);
             button8.TabIndex = 11;
@@ -161,29 +163,37 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(1464, 199);
+            pictureBox1.Location = new Point(17, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(243, 88);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 12;
             pictureBox1.TabStop = false;
             // 
+            // panel2
+            // 
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(button6);
+            panel2.Controls.Add(button8);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button1);
+            panel2.Controls.Add(button2);
+            panel2.Location = new Point(1658, 23);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(262, 653);
+            panel2.TabIndex = 13;
+            // 
             // AdminDashboard
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Teal;
-            ClientSize = new Size(1940, 1100);
-            Controls.Add(pictureBox1);
+            ClientSize = new Size(1920, 1080);
+            Controls.Add(panel2);
             Controls.Add(panel1);
-            Controls.Add(button7);
-            Controls.Add(button8);
-            Controls.Add(button1);
-            Controls.Add(button2);
-            Controls.Add(button3);
-            Controls.Add(button6);
-            Controls.Add(button4);
-            Controls.Add(button5);
             FormBorderStyle = FormBorderStyle.None;
             Name = "AdminDashboard";
             StartPosition = FormStartPosition.CenterScreen;
@@ -191,6 +201,7 @@
             WindowState = FormWindowState.Maximized;
             Load += Dashboard_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -205,5 +216,6 @@
         private Button button7;
         private Button button8;
         private PictureBox pictureBox1;
+        private Panel panel2;
     }
 }
