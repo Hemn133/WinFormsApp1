ee0095eaea3b47c087c56dd19e76e89071789504
Hemnn <hemnslemani18@gmail.com>
2025-01-14 07:17:54 +0300

    dddddd

--- ReturnAdmin.Designer.cs
+++ ReturnAdmin.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             label7 = new Label();
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            dataGridView2 = new DataGridView();
+            ExpenseAmount = new TextBox();
+            label3 = new Label();
+            button2 = new Button();
+            ProductSelection = new ComboBox();
+            label1 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label2 = new Label();
+            addtolist = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
             SuspendLayout();
             // 
             // label7
@@ -43,13 +59,182 @@
             label7.TabIndex = 36;
             label7.Text = "گەڕانەوەی کاڵا";
             // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 133);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(1610, 227);
+            dataGridView1.TabIndex = 51;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("NRT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(3, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(292, 33);
+            button1.TabIndex = 54;
+            button1.Text = "سڕینەوە";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.AllowUserToResizeColumns = false;
+            dataGridView2.AllowUserToResizeRows = false;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(3, 444);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.ReadOnly = true;
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView2.Size = new Size(1610, 273);
+            dataGridView2.TabIndex = 55;
+            // 
+            // ExpenseAmount
+            // 
+            ExpenseAmount.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ExpenseAmount.Location = new Point(1319, 94);
+            ExpenseAmount.Multiline = true;
+            ExpenseAmount.Name = "ExpenseAmount";
+            ExpenseAmount.Size = new Size(118, 33);
+            ExpenseAmount.TabIndex = 57;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Teal;
+            label3.Location = new Point(1443, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(168, 39);
+            label3.TabIndex = 56;
+            label3.Text = "ژمارەی پسوڵە";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("NRT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Teal;
+            button2.Location = new Point(1200, 94);
+            button2.Name = "button2";
+            button2.Size = new Size(113, 33);
+            button2.TabIndex = 58;
+            button2.Text = "گەڕان";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // ProductSelection
+            // 
+            ProductSelection.AutoCompleteMode = AutoCompleteMode.Suggest;
+            ProductSelection.AutoCompleteSource = AutoCompleteSource.ListItems;
+            ProductSelection.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ProductSelection.FormattingEnabled = true;
+            ProductSelection.Location = new Point(1378, 405);
+            ProductSelection.Name = "ProductSelection";
+            ProductSelection.Size = new Size(235, 33);
+            ProductSelection.TabIndex = 59;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Teal;
+            label1.Location = new Point(1559, 363);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 39);
+            label1.TabIndex = 60;
+            label1.Text = "کاڵا";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            numericUpDown1.Location = new Point(1271, 405);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(101, 33);
+            numericUpDown1.TabIndex = 62;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("NRT Bold", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Teal;
+            label2.Location = new Point(1314, 363);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 39);
+            label2.TabIndex = 61;
+            label2.Text = "دانە";
+            // 
+            // addtolist
+            // 
+            addtolist.Font = new Font("NRT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            addtolist.ForeColor = Color.Teal;
+            addtolist.Location = new Point(1115, 405);
+            addtolist.Name = "addtolist";
+            addtolist.Size = new Size(150, 33);
+            addtolist.TabIndex = 63;
+            addtolist.Text = "زیادکردن بۆ گەڕانەوە";
+            addtolist.UseVisualStyleBackColor = true;
+            addtolist.Click += addtolist_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("NRT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Teal;
+            button3.Location = new Point(3, 723);
+            button3.Name = "button3";
+            button3.Size = new Size(292, 33);
+            button3.TabIndex = 64;
+            button3.Text = "سڕینەوە";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("NRT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Teal;
+            button4.Location = new Point(1321, 723);
+            button4.Name = "button4";
+            button4.Size = new Size(292, 33);
+            button4.TabIndex = 65;
+            button4.Text = "گەڕانەوە";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
             // ReturnAdmin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(addtolist);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(ProductSelection);
+            Controls.Add(button2);
+            Controls.Add(ExpenseAmount);
+            Controls.Add(label3);
+            Controls.Add(dataGridView2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
             Controls.Add(label7);
             Name = "ReturnAdmin";
             Size = new Size(1616, 1029);
+            Load += ReturnAdmin_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +242,18 @@
         #endregion
 
         private Label label7;
+        private DataGridView dataGridView1;
+        private Button button1;
+        private DataGridView dataGridView2;
+        private TextBox ExpenseAmount;
+        private Label label3;
+        private Button button2;
+        private ComboBox ProductSelection;
+        private Label label1;
+        private NumericUpDown numericUpDown1;
+        private Label label2;
+        private Button addtolist;
+        private Button button3;
+        private Button button4;
     }
 }
--- ReturnAdmin.cs
+++ ReturnAdmin.cs
@@ -16,5 +16,330 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+        private void style(DataGridView datagridview)
+        {
+            datagridview.ColumnHeadersDefaultCellStyle.Font = new Font("NRT Bold", 12, FontStyle.Regular); // Adjust size if needed
+            datagridview.ColumnHeadersDefaultCellStyle.BackColor = Color.Teal; // Set background color to teal
+            datagridview.ColumnHeadersDefaultCellStyle.ForeColor = Color.White; // Set text color to white for better contrast
+            datagridview.AllowUserToAddRows = false;
+            datagridview.DefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            datagridview.RowTemplate.Height = 40;
+            datagridview.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            datagridview.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            datagridview.EnableHeadersVisualStyles = false;
+            datagridview.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            datagridview.AlternatingRowsDefaultCellStyle.BackColor = Color.LightGray;
+            datagridview.RowsDefaultCellStyle.BackColor = Color.White;
+            datagridview.BorderStyle = BorderStyle.Fixed3D;
+            datagridview.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            datagridview.GridColor = Color.Gray;
+            datagridview.DefaultCellStyle.SelectionBackColor = Color.DarkBlue;
+            datagridview.DefaultCellStyle.SelectionForeColor = Color.White;
+            ReverseColumnsOrder(datagridview);
+        }
+
+        private void ReverseColumnsOrder(DataGridView dataGridView)
+        {
+            int columnCount = dataGridView.Columns.Count;
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                dataGridView.Columns[i].DisplayIndex = columnCount - 1 - i;
+            }
+        }
+
+        private void ReturnAdmin_Load(object sender, EventArgs e)
+        {
+            style(dataGridView1);
+            style(dataGridView2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(ExpenseAmount.Text, out int saleId))
+            {
+                LoadSaleDetails(saleId);
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid Sale ID.");
+            }
+        }
+
+        private void ProcessReturn(int saleId, string productName, int returnQuantity)
+        {
+            // Get ProductID based on ProductName
+            string queryProductID = "SELECT ProductID FROM Product WHERE ProductName = @ProductName";
+            Dictionary<string, object> parametersProduct = new Dictionary<string, object>
+    {
+        { "@ProductName", productName }
+    };
+
+            DB db = new DB();
+            object productIdObj = db.ExecuteScalar(queryProductID, parametersProduct);
+
+            if (productIdObj == null)
+            {
+                MessageBox.Show("Product not found.");
+                return;
+            }
+
+            int productId = Convert.ToInt32(productIdObj);
+
+            // Update the SaleDetails table for the return
+            string updateQuery = @"
+        UPDATE SaleDetails
+        SET ReturnedQuantity = ReturnedQuantity + @ReturnQuantity
+        WHERE SaleID = @SaleID AND ProductID = @ProductID";
+
+            Dictionary<string, object> updateParameters = new Dictionary<string, object>
+    {
+        { "@ReturnQuantity", returnQuantity },
+        { "@SaleID", saleId },
+        { "@ProductID", productId }
+    };
+
+            db.ExecuteWithParameters(updateQuery, updateParameters);
+
+            MessageBox.Show("Product returned successfully.");
+
+            // Reload the updated sale details
+            LoadSaleDetails(saleId);
+
+            // Check if all products are fully returned
+            string checkQuery = @"
+        SELECT SUM(Quantity - ReturnedQuantity) AS RemainingQuantity
+        FROM SaleDetails
+        WHERE SaleID = @SaleID";
+
+            Dictionary<string, object> checkParameters = new Dictionary<string, object>
+    {
+        { "@SaleID", saleId }
+    };
+
+            object remainingQuantityObj = db.ExecuteScalar(checkQuery, checkParameters);
+            int remainingQuantity = remainingQuantityObj != null ? Convert.ToInt32(remainingQuantityObj) : 0;
+
+            if (remainingQuantity == 0)
+            {
+                // Mark Sale as fully returned
+                string markReturnedQuery = "UPDATE Sales SET IsReturned = 1 WHERE SaleID = @SaleID";
+                db.ExecuteWithParameters(markReturnedQuery, checkParameters);
+                MessageBox.Show("Sale fully returned.");
+            }
+        }
+
+
+
+
+        private void LoadSaleDetails(int saleId)
+        {
+            string query = @"
+        SELECT sd.ProductID, p.ProductName, sd.Quantity, sd.ReturnedQuantity 
+        FROM SalesDetails sd
+        JOIN Product p ON sd.ProductID = p.ProductID
+        WHERE sd.SaleID = @SaleID";
+
+            Dictionary<string, object> parameters = new Dictionary<string, object>
+    {
+        { "@SaleID", saleId }
+    };
+
+            DB db = new DB();
+            DataTable saleDetails = db.GetDataTableParam(query, parameters);
+
+            if (saleDetails.Rows.Count > 0)
+            {
+                // Bind to DataGridView
+                dataGridView1.DataSource = saleDetails;
+
+                // Populate ComboBox with product names
+                ProductSelection.Items.Clear();
+                foreach (DataRow row in saleDetails.Rows)
+                {
+                    ProductSelection.Items.Add(row["ProductName"].ToString());
+                }
+            }
+            else
+            {
+                MessageBox.Show("No sale found with this ID.");
+            }
+        }
+
+        private void addtolist_Click(object sender, EventArgs e)
+        {
+
+            if (dataGridView2.Columns.Count == 0)
+            {
+                dataGridView2.Columns.Add("ProductName", "Product Name");
+                dataGridView2.Columns.Add("Quantity", "Quantity");
+            }
+            // Get the selected product and quantity
+            string selectedProduct = ProductSelection.SelectedItem?.ToString();
+            int returnQuantity = (int)numericUpDown1.Value;
+
+            // Validate product selection
+            if (string.IsNullOrEmpty(selectedProduct))
+            {
+                MessageBox.Show("Please select a product.");
+                return;
+            }
+
+            // Validate quantity
+            if (returnQuantity <= 0)
+            {
+                MessageBox.Show("Please specify a valid quantity greater than 0.");
+                return;
+            }
+
+            // Get the available quantity of the selected product from DataGridView 1
+            int availableQuantity = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells["ProductName"].Value.ToString() == selectedProduct)
+                {
+                    availableQuantity = Convert.ToInt32(row.Cells["Quantity"].Value);
+                    break;
+                }
+            }
+
+            if (returnQuantity > availableQuantity)
+            {
+                MessageBox.Show("Return quantity exceeds the available quantity.");
+                return;
+            }
+
+            // Check if the product already exists in DataGridView 2
+            bool productExists = false;
+            foreach (DataGridViewRow row in dataGridView2.Rows)
+            {
+                if (row.Cells["ProductName"].Value.ToString() == selectedProduct)
+                {
+                    // Update the quantity
+                    int existingQuantity = Convert.ToInt32(row.Cells["Quantity"].Value);
+                    row.Cells["Quantity"].Value = existingQuantity + returnQuantity;
+                    productExists = true;
+                    break;
+                }
+            }
+
+            // If the product does not exist, add it as a new row
+            if (!productExists)
+            {
+                dataGridView2.Rows.Add(selectedProduct, returnQuantity);
+            }
+
+            MessageBox.Show("Product added to the return list.");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.Rows.Count == 0)
+            {
+                MessageBox.Show("No items in the return list.");
+                return;
+            }
+
+            DB db = new DB();
+
+            try
+            {
+                // Fetch SaleID from the TextBox instead of DataGridView1
+                if (string.IsNullOrWhiteSpace(ExpenseAmount.Text))
+                {
+                    MessageBox.Show("Sale ID cannot be empty.");
+                    return;
+                }
+
+                int saleID;
+                if (!int.TryParse(ExpenseAmount.Text, out saleID))
+                {
+                    MessageBox.Show("Invalid Sale ID. Please enter a valid number.");
+                    return;
+                }
+
+                foreach (DataGridViewRow row in dataGridView2.Rows)
+                {
+                    string productName = row.Cells["ProductName"].Value.ToString();
+                    int returnQuantity = Convert.ToInt32(row.Cells["Quantity"].Value);
+
+                    // Update the SaleDetails table
+                    string updateSaleDetailsQuery = @"
+                UPDATE SalesDetails 
+                SET Quantity = Quantity - @ReturnQuantity 
+                WHERE SaleID = @SaleID AND ProductName = @ProductName";
+                    var saleDetailsParams = new Dictionary<string, object>
+            {
+                { "@ReturnQuantity", returnQuantity },
+                { "@SaleID", saleID },
+                { "@ProductName", productName }
+            };
+                    db.ExecuteWithParameters(updateSaleDetailsQuery, saleDetailsParams);
+
+                    // Update the Product table to increase stock
+                    string updateStockQuery = "UPDATE Product SET QuantityAvailable = QuantityAvailable + @ReturnQuantity WHERE ProductName = @ProductName";
+                    var stockParams = new Dictionary<string, object>
+            {
+                { "@ReturnQuantity", returnQuantity },
+                { "@ProductName", productName }
+            };
+                    db.ExecuteWithParameters(updateStockQuery, stockParams);
+                }
+
+                MessageBox.Show("The selected items have been returned successfully.");
+                dataGridView2.Rows.Clear(); // Clear the return list after processing
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while processing the return: {ex.Message}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Get the SaleID from DataGridView1
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("No sale to return.");
+                return;
+            }
+
+            int saleID = Convert.ToInt32(dataGridView1.Rows[0].Cells["SaleID"].Value);
+
+            try
+            {
+                // Update the database to mark the sale as returned
+                string updateSaleQuery = "UPDATE Sales SET IsReturned = 1 WHERE SaleID = @SaleID";
+                var parameters = new Dictionary<string, object>
+        {
+            { "@SaleID", saleID }
+        };
+                DB db = new DB();
+                db.ExecuteWithParameters(updateSaleQuery, parameters);
+
+                // Update stock quantities for all products in the sale
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    string productName = row.Cells["ProductName"].Value.ToString();
+                    int quantity = Convert.ToInt32(row.Cells["Quantity"].Value);
+
+                    string updateStockQuery = "UPDATE Product SET QuantityAvailable = QuantityAvailable + @Quantity WHERE ProductName = @ProductName";
+                    var stockParams = new Dictionary<string, object>
+            {
+                { "@Quantity", quantity },
+                { "@ProductName", productName }
+            };
+                    db.ExecuteWithParameters(updateStockQuery, stockParams);
+                }
+
+                MessageBox.Show("The entire sale has been returned successfully.");
+                dataGridView1.Rows.Clear(); // Clear the DataGridView after processing
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while returning the sale: {ex.Message}");
+            }
+        }
     }
+
 }
